13250744d73ec77ed4b9bb3d6cd85f2c39a56971
Francesco Alizzi <alizzifra@gmail.com>
2025-11-19 15:13:30 +0100

    fix of players' models

--- src/players/models.py
+++ src/players/models.py
@@ -16,13 +16,14 @@ def utcnow():
 
 class PlayerProfile(db.Model):
     __tablename__ = "players"
+    
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, unique=True, nullable=False, index=True)
     username: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
-    #user_id: Mapped[int] = mapped_column(Integer, unique=True, nullable=False, index=True)
     #display_name: Mapped[str] = mapped_column(String(120), nullable=False)
     profile_picture: Mapped[str] = mapped_column(String(255), nullable=True)
-    nation: Mapped[str] = mapped_column(String(20), nullable=True)
+    region: Mapped[str] = mapped_column(String(20), nullable=True)
     created_at: Mapped[datetime] = mapped_column(
         DateTime, nullable=False, default=utcnow
     )
@@ -32,30 +33,30 @@ class PlayerProfile(db.Model):
 
     def __init__(
         self,
+        user_id: int,
         username: str,
-        #user_id: int,
         #display_name: str,
         profile_picture: str | None = None,
-        nation: str | None = None,
+        region: str | None = None,
         created_at: datetime | None = None,
         updated_at: datetime | None = None,
     ):
+        self.user_id = user_id
         self.username = username
-        #self.user_id = user_id
         #self.display_name = display_name
         self.profile_picture = profile_picture
-        self.nation = nation
+        self.region = region
         now = utcnow()
         self.created_at = created_at or now
         self.updated_at = updated_at or now
 
     def to_dict(self) -> dict:
         return {
+            "user_id": self.user_id,
             "username": self.username,
-            #"user_id": self.user_id,
             #"display_name": self.display_name,
             "profile_picture": self.profile_picture,
-            "nation": self.nation,
+            "region": self.region,
             "created_at": self.created_at.isoformat(),
             "updated_at": self.updated_at.isoformat(),
         }
