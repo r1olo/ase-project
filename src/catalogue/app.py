29f30c130b83d3b0c8c004294797ae8583f828ce
Rebecca Rodi <rebecca.rodi99@gmail.com>
2025-11-20 18:15:38 +0100

    added database init for cards

--- src/catalogue/app.py
+++ src/catalogue/app.py
@@ -1,13 +1,32 @@
 """Application factory for the Card Catalogue microservice."""
 from __future__ import annotations
 from .config import Config, TestConfig
+from .models import Card
 from .routes import catalogue as catalogue_blueprint
 from common.app_factory import create_flask_app
 from common.extensions import db
 from flask import Flask
 
+import json
+
 def _init_cards_db():
-    pass
+    with open("cards/cards.json") as file:
+        cards_data = json.load(file)
+        for _, card_info in cards_data.items():
+            card = Card.query.filter_by(name=card_info["name"]).first()
+            if card is not None:
+                continue
+            card = Card(
+                name=card_info["name"],
+                image=card_info["image"],
+                economy=card_info["economy"],
+                food=card_info["food"],
+                environment=card_info["environment"],
+                special=card_info["special"],
+                total=card_info["total"],
+            )
+            db.session.add(card)
+    db.session.commit()                
 
 def _create_app(config) -> Flask:
     return create_flask_app(
