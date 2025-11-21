1b948e278e24d59a30119c73e036e0717e476c06
Andrea Riolo <andry06082002@gmail.com>
2025-11-21 12:57:24 +0100

    added jwt dep in game engine

--- src/game_engine/app.py
+++ src/game_engine/app.py
@@ -1,27 +1,23 @@
 from flask import Flask
 from .config import Config, TestConfig
 from common.app_factory import create_flask_app
-from common.extensions import db
+from common.extensions import db, jwt
 from .routes import game_engine as game_blueprint 
-
 
 def _create_app(config_object) -> Flask:
     return create_flask_app(
         name=__name__,
         config_obj=config_object,
-        extensions=(db,),
+        extensions=(db, jwt),
         blueprints=(game_blueprint,),
         init_app_context_steps=(lambda _app: db.create_all(),),
     )
 
-
 def create_app() -> Flask:
     return _create_app(Config())
 
-
 def create_test_app() -> Flask:
     return _create_app(TestConfig())
-
 
 if __name__ == "__main__":  # pragma: no cover
     app = create_app()
