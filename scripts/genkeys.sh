8d7c18502ab8fce7fc32eb3f0b8bdde08d9a603e
Andrea Riolo <andry06082002@gmail.com>
2025-12-03 12:28:54 +0100

    change script genkeys.sh

--- scripts/genkeys.sh
+++ scripts/genkeys.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
-mkdir secrets
+FOLDER=secrets
+SERVICES=(
+    auth{,-db}
+    players{,-db}
+    game_engine{,-db}
+    matchmaking{,-db}
+    catalogue{,-db}
+)
 
-openssl genrsa -out secrets/jwtRS256.key 2048
+echo "Generating certificates and keys..."
+{
+    mkdir -p $FOLDER
 
-openssl rsa -in secrets/jwtRS256.key -pubout -out secrets/jwtRS256.key.pub
+    openssl genrsa -out $FOLDER/jwtRS256.key 2048
+    openssl rsa -in secrets/jwtRS256.key -pubout -out $FOLDER/jwtRS256.key.pub
+
+    for ser in "${SERVICES[@]}"; do
+        openssl req -x509 -newkey rsa:4096 -nodes -out "$FOLDER/$ser.crt" \
+            -keyout "$FOLDER/$ser.key" -days 365 -subj "/CN=$ser" \
+            -addext "subjectAltName=DNS:$ser,DNS:localhost,IP:127.0.0.1"
+    done
+} &>/dev/null && echo "Certificates and keys generated"
