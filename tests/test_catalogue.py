840d62565f3f95216d12c2c042aaa0f0b91464c3
Rebecca Rodi <rebecca.rodi99@gmail.com>
2025-12-14 22:10:42 +0100

    updated test_catalogue

--- tests/test_catalogue.py
+++ tests/test_catalogue.py
@@ -1,37 +1,11 @@
 from catalogue.models import Card
-from common.extensions import db
 
 import json
 
-# fill the database with cards from the json file
-def fill_db():
-    with open("cards/cards.json") as file:
-        cards_data = json.load(file)
-        for _, card_info in cards_data.items():
-            card = Card(
-                name=card_info["name"],
-                image=card_info["image"],
-                economy=card_info["economy"],
-                food=card_info["food"],
-                environment=card_info["environment"],
-                special=card_info["special"],
-                total=card_info["total"],
-            )
-            db.session.add(card)
-    db.session.commit()
-
 ### test cases for catalogue cards endpoints
 
-def test_get_all_cards_empty_db(catalogue_client):
-    # ensure no cards in the database
-    resp = catalogue_client.get("/cards")
-    assert resp.status_code == 200
-    data = resp.get_json()
-    assert data["data"] == []
-
-def test_get_all_cards(catalogue_client):
-    # add cards to the database
-    fill_db()
+def test_get_all_cards(disable_jwt, catalogue_client):
+    disable_jwt()
 
     # try to fetch all cards
     resp = catalogue_client.get("/cards")
@@ -45,68 +19,60 @@ def test_get_all_cards(catalogue_client):
             card_data.pop("id")  # remove id for comparison
             assert card_data in cards.values()
  
-def test_get_single_card_success(catalogue_client):
-    # add a card to the database
-    card = Card(
-        name="Molise",
-        image="/images/11-molise.png",
-        economy=4,
-        food=2,
-        environment=3,
-        special=8,
-        total=17.0
-    )
-    db.session.add(card)
-    db.session.commit()
+def test_get_single_card_success(disable_jwt, catalogue_client):
+    disable_jwt()
 
     # try to fetch the card by id
-    resp = catalogue_client.get(f"/cards/{card.id}")
+    resp = catalogue_client.get(f"/cards/8")
     assert resp.status_code == 200
     data = resp.get_json()
-    assert data["name"] == "Molise"
+    assert data["name"] == "Liguria"
 
-def test_get_single_card_not_found(catalogue_client):
+def test_get_single_card_not_found(disable_jwt, catalogue_client):
+    disable_jwt()
+
     # try to fetch a non-existent card
     resp = catalogue_client.get("/cards/123456789")
     assert resp.status_code == 404
     data = resp.get_json()
     assert data["msg"] == "Card not found"
 
-def test_get_single_card_invalid_id(catalogue_client):
+def test_get_single_card_invalid_id(disable_jwt, catalogue_client):
+    disable_jwt()
+
     # try to fetch a card using an invalid ID
     resp = catalogue_client.get("/cards/abc123")
-    assert resp.status_code == 400
-    data = resp.get_json()
-    assert data["msg"] == "Invalid card ID"
+    assert resp.status_code == 404
 
-def test_cards_validation_empty_body(catalogue_client):
+def test_cards_validation_empty_body(disable_jwt, catalogue_client):
+    disable_jwt()
+
     # try to validate an empty payload
     resp = catalogue_client.get("/cards/validation", json={})
     assert resp.status_code == 200
     data = resp.get_json()
     assert data["data"] == True
 
-def test_cards_validation_success(catalogue_client):
-    # add cards to the database
-    fill_db()
+def test_cards_validation_success(disable_jwt, catalogue_client):
+    disable_jwt()
 
-    data = []
-    data.append(Card.query.filter_by(name="Campania").first())
-    data.append(Card.query.filter_by(name="Lazio").first())
-    data.append(Card.query.filter_by(name="Liguria").first())
-    data.append(Card.query.filter_by(name="Sicilia").first())
-    data.append(Card.query.filter_by(name="Molise").first())
-    data.append(Card.query.filter_by(name="Veneto").first())
-    data.append(Card.query.filter_by(name="Trentino-Alto Adige").first())
+    data = [
+        Card.query.filter_by(name="Campania").first(),
+        Card.query.filter_by(name="Lazio").first(),
+        Card.query.filter_by(name="Liguria").first(),
+        Card.query.filter_by(name="Sicilia").first(),
+        Card.query.filter_by(name="Molise").first(),
+        Card.query.filter_by(name="Veneto").first(),
+        Card.query.filter_by(name="Trentino-Alto Adige").first()
+    ]
     
     resp = catalogue_client.get("/cards/validation", json={"data": [card.to_dict(relative=True) for card in data]})
     assert resp.status_code == 200
     data = resp.get_json()
     assert data["data"] == True
 
-def test_cards_validation_failure(catalogue_client):
-    # add cards to the database
-    fill_db()
+def test_cards_validation_failure(disable_jwt, catalogue_client):
+    disable_jwt()
 
     with open("cards/cards.json") as file:
         cards = json.load(file)
